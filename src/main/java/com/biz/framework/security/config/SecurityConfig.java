5c45c8fdf886e6527d923bf44d8be55d8e8b9e34
서한진 <seohanjin@gsitm.com>
2024-10-10 16:15:39 +0900

    spring security - csrf 비활성화

--- src/main/java/com/biz/framework/security/config/SecurityConfig.java
+++ src/main/java/com/biz/framework/security/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)  // 권장되는 CSRF 비활성화 설정 방식
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/login*", "/css/**", "/js/**", "/images/**", "/assets/**", "/config/**", "/static/**").permitAll()  // 정적 자원 및 로그인 페이지 허용
                         .anyRequest().authenticated()
