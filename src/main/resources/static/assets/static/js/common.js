275bdeabbc30f2ea605e3c822ce38f00eec515e5
배현철 <baechul@gsitm.com>
2024-12-04 20:20:47 +0900

    정산승인관리 수정분 반영

--- src/main/resources/static/assets/static/js/common.js
+++ src/main/resources/static/assets/static/js/common.js
@@ -121,3 +121,10 @@ function formatDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
 }
+
+
+function isNumeric(str) {
+    if (typeof str != "string") return false // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
