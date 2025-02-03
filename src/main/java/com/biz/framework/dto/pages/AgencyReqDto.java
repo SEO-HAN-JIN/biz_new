cb12ffb66cd74c8d3f841219e4450cba1baefd73
SEO-HAN-JIN <seo970713@gmail.com>
2025-02-04 00:55:23 +0900

    대행사 정산요청 ing3 ..

--- src/main/java/com/biz/framework/dto/pages/AgencyReqDto.java
+++ src/main/java/com/biz/framework/dto/pages/AgencyReqDto.java
@@ -15,12 +15,17 @@ public class AgencyReqDto extends BaseDto {
     private String userId;
     private String agencyId;
     private String reqDate;
+    private String incentiveRate;
     private String prodName;
     private String inflowCnt;
     private String prodAmt;
     private String saleTotalAmt;
     private String expectAmt;
     private String applyStatus;
+
+    private String agencyName;
+    private String searchStartDate;
+    private String searchEndDate;
 
     private List<AgencyReqDto> agencyReqDtoList;
 
