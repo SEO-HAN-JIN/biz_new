fd0c8d31514af33048a1dd10f4dd3aa57d6107d4
서한진 <seohanjin@gsitm.com>
2025-02-20 09:15:20 +0900

    직원 삭제

--- src/main/java/com/biz/framework/service/pages/EmpService.java
+++ src/main/java/com/biz/framework/service/pages/EmpService.java
@@ -58,7 +58,6 @@ public class EmpService {
         UserDto userDto = new UserDto();
         userDto.setUserId(empDto.getEmpId());
 
-        result += userMapper.deleteUser(userDto);
         return result;
     }
 
